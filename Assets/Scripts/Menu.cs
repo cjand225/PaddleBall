2fd73920bb457f826acad417b8d8e1b6c33bf50f
James Andrews <cjand225@gmail.com>
2023-04-28 23:19:19 -0600

    Added documentation to menu script

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,17 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+/**
+ * @brief A class that manages the menu screen and its UI elements.
+ */
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    /**
+     * The UI button for exiting the game.
+     */
     private GameObject exitGameUIButton;
 
+    /**
+     * The name of the UI button for exiting the game.
+     */
     private string exitGameUIButtonName = "ButtonExit";
 
+    /**
+     * Find and remove or hide the UI element depending on the platform.
+     */
     void Start()
     {
+        // Find the UI button for exiting the game
         exitGameUIButton = GameObject.Find(exitGameUIButtonName);
 
         // Remove or hide the UI element depending on the platform
@@ -20,16 +31,25 @@ public class Menu : MonoBehaviour
         #endif
     }
 
+    /**
+     * Load the game scene.
+     */
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
     }
 
+    /**
+     * Load the menu scene.
+     */
     public void ExitCurrentSession()
     {
         SceneManager.LoadScene("MenuScene");
     }
 
+    /**
+     * Exit the game.
+     */
     public void ExitGame()
     {
         Application.Quit();
