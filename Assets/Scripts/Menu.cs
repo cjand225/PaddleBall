8cb180c069b1820c20e274ba554893252c536cf7
James Andrews <cjand225@gmail.com>
2023-04-28 21:51:44 -0600

    Remove Exit button on start if WebGL

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,9 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    private GameObject exitGameUIButton;
+
+    private string exitGameUIButtonName = "ButtonExit";
+
+    void Start()
+    {
+        exitGameUIButton = GameObject.Find(exitGameUIButtonName);
+
+        // Remove or hide the UI element depending on the platform
+        #if UNITY_WEBGL
+            Destroy(exitGameUIButton);
+        #endif
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
